cc5e90f055cb8b7cd1fbe121c86aef5167977f5b
3wt58io <160789384+3wt58io@users.noreply.github.com>
2024-10-01 16:14:58 +0300

    Update Terminal.py

--- Terminal.py
+++ Terminal.py
@@ -15,21 +15,11 @@ def main():
     start = True
     while start:
       codename = extract_last_segment(code)
-      if cmd == "r":
-         codename = input("new name: ")
-         os.system(f"mv {extract_last_segment(code)} {codename}")
-      elif cmd == "d":
-          os.system(f"rm {codename}")
-      elif cmd == "run":
-          os.system(f"wget {code}")
-      if codename != extract_last_segment(code):
-          
-          os.system(f"rm {codename}")
-          os.system(f"mv {extract_last_segment(code)} {codename}")
-          os.system(f"python3 {codename}")
-      elif cmd == "end":
-          start = False
-      os.system("sync")   
+      if cmd == "run":
+         os.system(f"wget {code}")
+         os.system(f"python3 {codename}")
+         
+       os.system("sync")   
 # Make sure to call the main function
 if __name__ == "__main__":
     main()
